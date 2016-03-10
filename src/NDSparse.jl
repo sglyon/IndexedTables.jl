b9ac49da1a4efe5687ac320b33e6f58c52244fdd
Jeff Bezanson <jeff.bezanson@gmail.com>
2016-03-10 18:12:45 -0500

    import code and license file

---
+++ src/NDSparse.jl
@@ -0,0 +1,558 @@
+using Base.Test
+import Base:
+    show, summary, eltype, length, sortperm, issorted, permute!, sort!,
+    getindex, setindex!, ndims, eachindex, size, union, intersect, map, convert,
+    linearindexing, ==, broadcast, empty!, copy, similar, sum
+
+include("utils.jl")
+
+immutable Indexes{D<:Tuple, C<:Tuple} <: AbstractVector{D}
+    columns::C
+
+    call(::Type{Indexes}, columns::AbstractVector...) =
+        new{eltypes(typeof(columns)),typeof(columns)}(columns)
+end
+
+eltype{D,C}(::Type{Indexes{D,C}}) = D
+dimensions{D,C}(::Type{Indexes{D,C}}) = D
+length(c::Indexes) = length(c.columns[1])
+ndims(c::Indexes) = length(c.columns)
+size(c::Indexes) = (length(c),)
+linearindexing{T<:Indexes}(::Type{T}) = Base.LinearFast()
+summary{D<:Tuple}(c::Indexes{D}) = "Indexes{$D}"
+
+empty!(c::Indexes) = (map(empty!, c.columns); c)
+similar(c::Indexes) = empty!(Indexes(map(similar, c.columns)...))
+copy(c::Indexes) = Indexes(map(copy, c.columns)...)
+
+row(c::Indexes, i) = Base.ith_all(i, c.columns)
+getindex(c::Indexes, i) = row(c, i)
+rowless(c::Indexes, i, j) = isless(row(c,i), row(c,j))
+
+function ==(x::Indexes, y::Indexes)
+    ndims(x) == ndims(y) || return false
+    n = length(x)
+    length(y) == n || return false
+    for i in 1:n
+        row(x,i) == row(y,i) || return false
+    end
+    return true
+end
+
+sortperm(c::Indexes) = sort!([1:length(c);], lt=(x,y)->rowless(c, x, y))
+issorted(c::Indexes) = issorted(1:length(c), lt=(x,y)->rowless(c, x, y))
+
+function permute!(c::Indexes, p::AbstractVector)
+    for v in c.columns
+        permute!(v, p)
+    end
+    return c
+end
+sort!(c::Indexes) = permute!(c, sortperm(c))
+
+let a = Indexes([1,2,1],["foo","bar","baz"]),
+    b = Indexes([2,1,1],["bar","baz","foo"]),
+    c = Indexes([1,1,2],["foo","baz","bar"])
+    @test a != b
+    @test a != c
+    @test b != c
+    @test sort!(a) == sort!(b) == sort!(c)
+end
+
+immutable NDSparse{T, D<:Tuple, C<:Tuple, V<:AbstractVector}
+    indexes::Indexes{D,C}
+    data::V
+    default::T
+
+    index_buffer::Indexes{D,C}
+    data_buffer::V
+end
+
+NDSparse(i::Indexes, d::AbstractVector, dflt) =
+    NDSparse(i, d, dflt, Indexes(map(c->similar(c, 0), i.columns)...), similar(d, 0))
+
+similar(t::NDSparse) =
+    NDSparse(similar(t.indexes), empty!(similar(t.data)), copy(t.default))
+
+function copy(t::NDSparse)
+    flush!(t)
+    NDSparse(copy(t.indexes), copy(t.data), copy(t.default))
+end
+
+function empty!(t::NDSparse)
+    empty!(t.indexes)
+    empty!(t.data)
+    empty!(t.index_buffer)
+    empty!(t.data_buffer)
+    return t
+end
+
+function show{T,D<:Tuple}(io::IO, t::NDSparse{T,D})
+    flush!(t)
+    print(io, "NDSparse{$T,$D}:")
+    n = length(t.indexes)
+    for i in 1:min(n,10)
+        println(io)
+        print(io, " $(row(t.indexes, i)) => $(t.data[i])")
+    end
+    if n > 20
+        println(); print(" ⋮")
+        for i in (n-9):n
+            println(io)
+            print(io, " $(row(t.indexes, i)) => $(t.data[i])")
+        end
+    end
+end
+
+immutable WithDefault{T}
+    data::Vector{T}
+    default::T
+end
+
+function NDSparse(columns...)
+    keys = columns[1:end-1]
+    data = columns[end]
+    if isa(data, WithDefault)
+        dflt = data.default
+        data = data.data
+    else
+        dflt = zero(eltype(data))
+    end
+    n = length(data)
+    for col in keys
+        length(col) == n || error("all columns must have same length")
+    end
+    indexes = Indexes(keys...)
+    if !issorted(indexes)
+        p = sortperm(indexes)
+        permute!(indexes, p)
+        permute!(data, p)
+    end
+    NDSparse(indexes, data, dflt)
+end
+
+ndims(t::NDSparse) = ndims(t.indexes)
+dimensions(t::NDSparse) = dimensions(t.indexes)
+length(t::NDSparse) = (flush!(t);length(t.indexes))
+eltype{T,D,C}(::Type{NDSparse{T,D,C}}) = T
+
+function sort!(t::NDSparse)
+    p = sortperm(t.indexes)
+    permute!(t.indexes, p)
+    permute!(t.data, p)
+    return t
+end
+
+getindex(t::NDSparse, idxs...) = (flush!(t); _getindex(t, idxs))
+
+_getindex{T,D<:Tuple}(t::NDSparse{T,D}, idxs::D) = _getindex_scalar(t, idxs)
+_getindex(t::NDSparse, idxs::Tuple{Vararg{Real}}) = _getindex_scalar(t, idxs)
+
+function _getindex_scalar(t, idxs)
+    i = searchsorted(t.indexes, idxs)
+    length(i) != 1 && return t.default
+    t.data[first(i)]
+end
+
+_in(x, y) = in(x, y)
+_in(x, ::Colon) = true
+_in(x, v::AbstractVector) = searchsortedfirst(v, x)<=length(v)
+_in(x, v::AbstractString) = x == v
+_in(x, v::Dimension) = x == v
+
+import Base: tail
+# test whether row r is within product(idxs...)
+#row_in(r::Tuple{}, idxs) = true
+row_in(r, idxs) = row_in(r[1], idxs[1], tail(r), tail(idxs))
+row_in(r1, i1, rr, ri) = _in(r1,i1) & row_in(rr[1], ri[1], tail(rr), tail(ri))
+row_in(r1, i1, rr::Tuple{}, ri) = _in(r1,i1)
+
+function countunique(C)
+    seen = Set{eltype(C)}()
+    for x in C
+        push!(seen, x)
+    end
+    length(seen)
+end
+
+length_estimate(col, idx::AbstractArray) = length(idx)
+length_estimate(col, idx) = 1
+length_estimate(col, idx::Colon) = 1
+function length_estimate(col, idx::Interval)
+    count = 0
+    for i = 1:length(col)
+        x = col[i]
+        if i>1 && isless(x,col[i-1])
+            break
+        end
+        if x in idx
+            count += 1
+        end
+    end
+    return max(1,count)
+end
+
+# TODO: `countunique` gives a better size estimate but takes quite a while
+#    countunique(t.indexes.columns[n])
+idxlen(t::NDSparse, n::Int, i1, irest...) =
+    length_estimate(t.indexes.columns[n],i1) * idxlen(t, n+1, irest...)
+idxlen(t::NDSparse, n::Int) = 1
+idxlen(t::NDSparse, idxs) = idxlen(t, 1, idxs...)
+
+range_estimate(col, idx) = 1:length(col)
+range_estimate(col, idx::AbstractArray) = searchsortedfirst(col,first(idx)):searchsortedlast(col,last(idx))
+
+pushrow!(I::Indexes, r) = _pushrow!(I.columns[1], r[1], tail(I.columns), tail(r))
+_pushrow!(c1, r1, cr, rr) = (push!(c1, r1); _pushrow!(cr[1], rr[1], tail(cr), tail(rr)))
+_pushrow!(c1, r1, cr::Tuple{}, rr) = push!(c1, r1)
+
+function _getindex(t::NDSparse, idxs)
+    if length(idxs) != length(t.indexes.columns)
+        error("wrong number of indexes")
+    end
+    I = t.indexes
+    lI = length(I)
+    guess = min(idxlen(t,idxs), lI)
+    K = Indexes(map(c->sizehint!(similar(c,0),guess), I.columns)...)::typeof(I)
+    td = t.data
+    data = sizehint!(similar(t.data,0),guess)
+    for i in range_estimate(I.columns[1], idxs[1])
+        ri = I[i]
+        if row_in(ri, idxs)
+            pushrow!(K, ri)
+            push!(data, td[i])
+        end
+    end
+    NDSparse(K, data, t.default)
+end
+
+setindex!(t::NDSparse, rhs, idxs...) = _setindex!(t, rhs, idxs)
+
+_setindex!{T,D}(t::NDSparse{T,D}, rhs::AbstractArray, idxs::D) = _setindex_scalar!(t, rhs, idxs)
+_setindex!(t::NDSparse, rhs::AbstractArray, idxs::Tuple{Vararg{Real}}) = _setindex_scalar!(t, rhs, idxs)
+_setindex!{T,D}(t::NDSparse{T,D}, rhs, idxs::D) = _setindex_scalar!(t, rhs, idxs)
+#_setindex!(t::NDSparse, rhs, idxs::Tuple{Vararg{Real}}) = _setindex_scalar!(t, rhs, idxs)
+
+function _setindex_scalar!(t, rhs, idxs)
+    pushrow!(t.index_buffer, idxs)
+    push!(t.data_buffer, rhs)
+    t
+end
+
+_setindex!(t::NDSparse, rhs::NDSparse, idxs::Tuple{Vararg{Real}}) = _setindex!(t, rhs.data, idxs)
+_setindex!(t::NDSparse, rhs::NDSparse, idxs) = _setindex!(t, rhs.data, idxs)
+
+@inline function fixi(t::NDSparse, n::Int, i1::Colon, irest...)
+    u = unique(t.indexes.columns[n])
+    (n==1 ? u : sort(u), fixi(t, n+1, irest...)...)
+end
+
+fixi(t::NDSparse, n::Int, i1, irest...) = (i1, fixi(t, n+1, irest...)...)
+fixi(t::NDSparse, n::Int) = ()
+fixi(t::NDSparse, idxs) = fixi(t, 1, idxs...)
+
+_setindex!{T,D}(t::NDSparse{T,D}, rhs::AbstractArray, idxs) =
+    _setindex_array_range!(t, rhs, fixi(t, idxs))
+
+function _setindex_array_range!{T,D}(t::NDSparse{T,D}, rhs::AbstractArray, idxs)
+    # TODO performance
+    for (x,I) in zip(rhs,product(idxs...))
+        _setindex!(t, x, convert(D, I))
+    end
+end
+
+function _setindex!{T,D}(t::NDSparse{T,D}, rhs, idxs)
+    # TODO performance
+    for I in product(idxs...)
+        _setindex!(t, rhs, convert(D, I))
+    end
+end
+
+# sort and merge data from accumulation buffer
+function flush!(t::NDSparse)
+    if !isempty(t.data_buffer)
+        # 1. sort the buffer
+        p = sortperm(t.index_buffer)
+        permute!(t.index_buffer, p)
+        permute!(t.data_buffer, p)
+
+        # 2. merge to a new copy
+        new = _merge(t, NDSparse(t.index_buffer, t.data_buffer, t.default))
+
+        # 3. resize and copy data into t
+        for i = 1:length(t.indexes.columns)
+            resize!(t.indexes.columns[i], length(new.indexes.columns[i]))
+            copy!(t.indexes.columns[i], new.indexes.columns[i])
+        end
+        resize!(t.data, length(new.data))
+        copy!(t.data, new.data)
+
+        # 4. clear buffer
+        for c in t.index_buffer.columns; empty!(c); end
+        empty!(t.data_buffer)
+    end
+    nothing
+end
+
+function union{D}(I::Indexes{D}, J::Indexes{D})
+    lI, lJ = length(I), length(J)
+    guess = max(lI, lJ)
+    K = Indexes(map(c->sizehint!(similar(c,0),guess), I.columns)...)::typeof(I)
+    i = j = 1
+    while true
+        if i <= lI && j <= lJ
+            ri, rj = I[i], J[j]
+            c = cmp(ri, rj)
+            if c == 0
+                pushrow!(K, ri)
+                i += 1
+                j += 1
+            elseif c < 0
+                pushrow!(K, ri)
+                i += 1
+            else
+                pushrow!(K, rj)
+                j += 1
+            end
+        elseif i <= lI
+            pushrow!(K, I[i])
+            i += 1
+        elseif j <= lJ
+            pushrow!(K, J[j])
+            j += 1
+        else
+            break
+        end
+    end
+    return K
+end
+
+let c = Indexes([1,1,1,2,2], [1,2,4,3,5]),
+    d = Indexes([1,1,2,2,2], [1,3,1,4,5]),
+    e = Indexes([1,1,1], sort([rand(),0.5,rand()])),
+    f = Indexes([1,1,1], sort([rand(),0.5,rand()]))
+    @test union(c,d) == Indexes([1,1,1,1,2,2,2,2],[1,2,3,4,1,3,4,5])
+    @test length(union(e,f).columns[1]) == 5
+end
+
+function intersect{D}(I::Indexes{D}, J::Indexes{D})
+    lI, lJ = length(I), length(J)
+    guess = min(lI, lJ)
+    K = Indexes(map(c->sizehint!(similar(c,0),guess), I.columns)...)::typeof(I)
+    i = j = 1
+    while i <= lI && j <= lJ
+        ri, rj = I[i], J[j]
+        c = cmp(ri, rj)
+        if c == 0
+            pushrow!(K, ri)
+            i += 1
+            j += 1
+        elseif c < 0
+            i += 1
+        else
+            j += 1
+        end
+    end
+    return K
+end
+
+let c = Indexes([1,1,1,2,2], [1,2,4,3,5]),
+    d = Indexes([1,1,2,2,2], [1,3,1,4,5]),
+    e = Indexes([1,1,1], sort([rand(),0.5,rand()])),
+    f = Indexes([1,1,1], sort([rand(),0.5,rand()]))
+    @test intersect(c,d) == Indexes([1,2],[1,5])
+    @test length(intersect(e,f).columns[1]) == 1
+end
+
+# assign y into x out-of-place
+merge{T,S,D}(x::NDSparse{T,D}, y::NDSparse{S,D}) = (flush!(x);flush!(y); _merge(x, y))
+# merge without flush!
+function _merge{T,S,D}(x::NDSparse{T,D}, y::NDSparse{S,D})
+    K = union(x.indexes, y.indexes)
+    n = length(K)
+    lx, ly = length(x.indexes), length(y.indexes)
+    dflt = x.default
+    data = Vector{typeof(dflt)}(n)
+    i = j = 1
+    for k = 1:n
+        r = K[k]
+        if j <= ly && r == y.indexes[j]
+            data[k] = y.data[j]
+            j += 1
+            if i <= lx && r == x.indexes[i]
+                i += 1
+            end
+        elseif i <= lx
+            data[k] = x.data[i]
+            i += 1
+        end
+    end
+    NDSparse(K, data, dflt)
+end
+
+function map{T,S,D}(f, x::NDSparse{T,D}, y::NDSparse{S,D})
+    flush!(x); flush!(y)
+    K = union(x.indexes, y.indexes)
+    n = length(K)
+    lx, ly = length(x.indexes), length(y.indexes)
+    dflt = f(x.default, y.default)
+    data = Vector{typeof(dflt)}(n)
+    i = j = 1
+    for k = 1:n
+        r = K[k]
+        xval, yval = x.default, y.default
+        if i <= lx && r == x.indexes[i]
+            xval = x.data[i]
+            i += 1
+        end
+        if j <= ly && r == y.indexes[j]
+            yval = y.data[j]
+            j += 1
+        end
+        data[k] = f(xval, yval)
+    end
+    NDSparse(K, data, dflt)
+end
+
+map(f, x::NDSparse) = NDSparse(x.indexes, map(f, x.data), f(x.default))
+
+tslice(t::Tuple, I) = ntuple(i->t[I[i]], length(I))
+
+function match_indices(A::NDSparse, B::NDSparse)
+    Ap = typeof(A).parameters[2].parameters
+    Bp = typeof(B).parameters[2].parameters
+    matches = zeros(Int, length(Ap))
+    J = IntSet(1:length(Bp))
+    for i = 1:length(Ap)
+        for j in J
+            if Ap[i] == Bp[j]
+                matches[i] = j
+                delete!(J, j)
+                break
+            end
+        end
+    end
+    isempty(J) || error("unmatched source indices: $(collect(J))")
+    tuple(matches...)
+end
+
+function broadcast!(f::Function, A::NDSparse, B::NDSparse, C::NDSparse)
+    A.default == f(B.default, C.default) ||
+        error("destination default value doesn't match")
+    flush!(A); flush!(B); flush!(C)
+    B_inds = match_indices(A, B)
+    C_inds = match_indices(A, C)
+    all(i->B_inds[i] > 0 || C_inds[i] > 0, 1:ndims(A)) ||
+        error("some destination indices are uncovered")
+    common = filter(i->B_inds[i] > 0 && C_inds[i] > 0, 1:ndims(A))
+    B_common = tslice(B_inds, common)
+    C_common = tslice(C_inds, common)
+    B_perm = sortperm(Indexes(B.indexes.columns[[B_common...]]...))
+    C_perm = sortperm(Indexes(C.indexes.columns[[C_common...]]...))
+    empty!(A)
+    m, n = length(B_perm), length(C_perm)
+    jlo = klo = 1
+    while jlo <= m && klo <= n
+        b_common = tslice(B.indexes[B_perm[jlo]], B_common)
+        c_common = tslice(C.indexes[C_perm[klo]], C_common)
+        x = cmp(b_common, c_common)
+        x < 0 && (jlo += 1; continue)
+        x > 0 && (klo += 1; continue)
+        jhi, khi = jlo + 1, klo + 1
+        while jhi <= m && tslice(B.indexes[B_perm[jhi]], B_common) == b_common
+            jhi += 1
+        end
+        while khi <= n && tslice(C.indexes[C_perm[khi]], C_common) == c_common
+            khi += 1
+        end
+        for ji = jlo:jhi-1
+            j = B_perm[ji]
+            b_row = B.indexes[j]
+            for ki = klo:khi-1
+                k = C_perm[ki]
+                c_row = C.indexes[k]
+                vals = ntuple(ndims(A)) do i
+                    B_inds[i] > 0 ? b_row[B_inds[i]] : c_row[C_inds[i]]
+                end
+                pushrow!(A.indexes, vals)
+                push!(A.data, f(B.data[j], C.data[k]))
+            end
+        end
+        jlo, klo = jhi, khi
+    end
+    sort!(A)
+end
+
+# TODO: allow B to subsume columns of A as well?
+
+broadcast(f::Function, A::NDSparse, B::NDSparse) = broadcast!(f, similar(A), A, B)
+
+broadcast(f::Function, x::NDSparse, y) = NDSparse(x.indexes, broadcast(f, x.data, y), f(x.default, y))
+broadcast(f::Function, y, x::NDSparse) = NDSparse(x.indexes, broadcast(f, y, x.data), f(y, x.default))
+
+convert(::Type{NDSparse}, m::SparseMatrixCSC) = NDSparse(findnz(m)[[2,1,3]]...)
+
+function convert{T}(::Type{NDSparse}, a::AbstractArray{T}; default::T = zero(eltype(a)))
+    n = 0
+    for x in a; if x!=default; n+=1; end; end
+    data = Vector{T}(n)
+    nd = ndims(a)
+    idxs = [ Vector{Int}(n) for i = 1:nd ]
+    i = 1
+    for I in CartesianRange(size(a))
+        val = a[I]
+        if val != default
+            for j = 1:nd
+                idxs[j][i] = I[j]
+            end
+            data[i] = val
+            i += 1
+        end
+    end
+    NDSparse(Indexes(reverse(idxs)...), data, default)
+end
+
+# combine adjacent rows with equal indexes using the given function
+function aggregate!(f, x::NDSparse)
+    idxs, data = x.indexes, x.data
+    n = length(idxs)
+    newlen = 1
+    current = idxs[newlen]
+    for i = 2:n
+        if idxs[i] == current
+            data[newlen] = f(data[newlen], data[i])
+        else
+            newlen += 1
+            if newlen != i
+                data[newlen] = data[i]
+                for c in idxs.columns
+                    c[newlen] = c[i]
+                end
+            end
+            current = idxs[newlen]
+        end
+    end
+    resize!(data, newlen)
+    for c in idxs.columns
+        resize!(c, newlen)
+    end
+    x
+end
+
+# convert dimension `d` of `x` using the given translation dictionary.
+# if the relation is many-to-one, aggregate with function `agg`
+function convert_dimension(x::NDSparse, d::Int, xlat::Dict, agg=+)
+    cols = x.indexes.columns
+    d2 = map(x->xlat[x], cols[d])
+    x2 = NDSparse(map(copy,cols[1:d-1])..., d2, map(copy,cols[d+1:end])..., copy(x.data))
+    aggregate!(agg, x2)
+end
+
+sum(x::NDSparse) = sum(x.data)
+
+srand(123)
+A = NDSparse(rand(1:3,10), rand('A':'F',10), map(UInt8,rand(1:3,10)), collect(1:10), randn(10))
+B = NDSparse(map(UInt8,rand(1:3,10)), rand('A':'F',10), rand(1:3,10), randn(10))
+C = NDSparse(map(UInt8,rand(1:3,10)), rand(1:3,10), rand(1:3,10), randn(10))
+
+nothing # silent loading
---
+++ src/utils.jl
@@ -0,0 +1,105 @@
+using Base.Test
+import Base: tuple_type_cons, tuple_type_head, tuple_type_tail, in, ==, isless, convert,
+             length, eltype, start, next, done, show
+
+eltypes(::Type{Tuple{}}) = Tuple{}
+eltypes{T<:Tuple}(::Type{T}) =
+    tuple_type_cons(eltype(tuple_type_head(T)), eltypes(tuple_type_tail(T)))
+
+immutable Interval{T,closed1,closed2}
+    lo::T
+    hi::T
+end
+
+Interval{T}(lo::T,hi::T) = Interval{T,true,true}(lo,hi)
+
+in{T}(x, i::Interval{T,false,false}) = i.lo <  x <  i.hi
+in{T}(x, i::Interval{T,true,false})  = i.lo <= x <  i.hi
+in{T}(x, i::Interval{T,false,true})  = i.lo <  x <= i.hi
+in{T}(x, i::Interval{T,true,true})   = i.lo <= x <= i.hi
+
+abstract Dimension{T<:Real}
+
+==(a::Dimension, b::Dimension) = a.value == b.value
+isless(a::Dimension, b::Dimension) = isless(a.value, b.value)
+# these convert definitions are ambiguous unless Dimension's
+# parameter is restricted (I picked Real)
+convert{T<:Real}(::Type{T}, d::Dimension{T}) = d.value
+convert{T<:Dimension}(::Type{T}, x::Real) = T(x)
+
+default{T}(D::Type{Dimension{T}}) = D(default(T))
+
+function show(io::IO, d::Dimension)
+    print(io, typeof(d).name, "(")
+    show(io, d.value)
+    print(io, ")")
+end
+
+macro dimension(d)
+    T = esc(:T); d = esc(d)
+    quote
+        immutable ($d){$T <: Real} <: Dimension{$T}
+            value::($T)
+        end
+    end
+end
+
+# dimension where all values x are lo <= x <= hi
+#abstract BoundedDimension{T, lo, hi} <: Dimension{T}
+
+# maybe: dimension where all values belong to some Range
+#abstract RangeDimension{T, r} <: Dimension{T}
+
+# lexicographic order product iterator
+
+import Base: length, eltype, start, next, done
+
+abstract AbstractProdIterator
+
+immutable Prod2{I1, I2} <: AbstractProdIterator
+    a::I1
+    b::I2
+end
+
+product(a) = a
+product(a, b) = Prod2(a, b)
+eltype{I1,I2}(::Type{Prod2{I1,I2}}) = Tuple{eltype(I1), eltype(I2)}
+length(p::AbstractProdIterator) = length(p.a)*length(p.b)
+
+function start(p::AbstractProdIterator)
+    s1, s2 = start(p.a), start(p.b)
+    s1, s2, (done(p.a,s1) || done(p.b,s2))
+end
+
+function prod_next(p, st)
+    s1, s2 = st[1], st[2]
+    v2, s2 = next(p.b, s2)
+    doneflag = false
+    if done(p.b, s2)
+        v1, s1 = next(p.a, s1)
+        if !done(p.a, s1)
+            s2 = start(p.b)
+        else
+            doneflag = true
+        end
+    else
+        v1, _ = next(p.a, s1)
+    end
+    return (v1,v2), (s1,s2,doneflag)
+end
+
+next(p::Prod2, st) = prod_next(p, st)
+done(p::AbstractProdIterator, st) = st[3]
+
+immutable Prod{I1, I2<:AbstractProdIterator} <: AbstractProdIterator
+    a::I1
+    b::I2
+end
+
+product(a, b, c...) = Prod(a, product(b, c...))
+eltype{I1,I2}(::Type{Prod{I1,I2}}) = tuple_type_cons(eltype(I1), eltype(I2))
+
+function next{I1,I2}(p::Prod{I1,I2}, st)
+    x = prod_next(p, st)
+    ((x[1][1],x[1][2]...), x[2])
+end
