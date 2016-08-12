bd9c8f94146d8b0ee7022947b39a31a84447cebc
Jeff Bezanson <jeff.bezanson@gmail.com>
2016-08-11 20:15:01 -0400

    fix type of `Columns` constructed from a NamedTuple of vectors

--- src/columns.jl
+++ src/columns.jl
@@ -34,7 +34,7 @@ Columns(; pairs...) = Columns(map(x->x[2],pairs)..., names=Symbol[x[1] for x in 
 (::Type{Columns{D}}){D}(columns::AbstractVector...) = Columns{D,typeof(columns)}(columns)
 
 Columns(c::Tuple) = Columns{eltypes(typeof(c)),typeof(c)}(c)
-Columns(c::NamedTuple) = Columns{Tuple{typeof(c).types...},typeof(c)}(c)
+Columns(c::NamedTuple) = Columns{eltypes(Tuple{typeof(c).types...}),typeof(c)}(c)
 
 eltype{D,C}(::Type{Columns{D,C}}) = D
 length(c::Columns) = length(c.columns[1])
