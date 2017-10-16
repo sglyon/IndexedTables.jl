b6e2e9d3d0d8b29a6599b9061a3de7d1ae8469d8
Shashi Gowda <shashi@juliacomputing.com>
2017-10-16 19:48:26 +0530

    outerjoin

--- src/table/join.jl
+++ src/table/join.jl
@@ -93,7 +93,7 @@ end
 function leftjoin(f, left::NextTable, right::NextTable;
                   lkey=pkeynames(left), rkey=pkeynames(right),
                   lselect=rows(left), rselect=rows(right))
-    
+
     lperm = sortpermby(left, lkey)
     rperm = sortpermby(right, rkey)
 
@@ -160,4 +160,82 @@ end
     @test leftjoin(t1, t2) == NextTable([1,2,3,4], [(1,5), (2,6), (3,6), (4,7)])
     @test leftjoin(+, t1, t3, lselect=2, rselect=2)  == NextTable([1,2,3,4,4],[NA,NA,13,15,16])
     @test leftjoin(+, t3, t4, lselect=2, rselect=2) == NextTable([0,3,4,4,4,4], [NA, 12, 14,15,15,16])
+end
+
+function outerjoin(f, left::NextTable, right::NextTable;
+                   lkey=pkeynames(left), rkey=pkeynames(right),
+                   lselect=rows(left), rselect=rows(right))
+
+    lperm = sortpermby(left, lkey)
+    rperm = sortpermby(right, rkey)
+
+    lkey = rows(left, lkey)
+    rkey = rows(right, rkey)
+
+    ldata = rows(left, lselect)
+    rdata = rows(right, rselect)
+
+    rT = eltype(rdata)
+    lT = eltype(ldata)
+    out_type = _promote_op(f, Union{DataValue{lT}, lT}, Union{DataValue{rT}, rT})
+    data = similar(arrayof(out_type), 0)
+
+    _outerjoin(f, data, lkey, rkey, ldata, rdata, lperm, rperm)
+end
+
+function _outerjoin(f, data, lkey, rkey, ldata, rdata, lperm, rperm)
+    ll, rr = length(lkey), length(rkey)
+
+    I = _sizehint!(similar(lkey,0), length(lkey))
+    _sizehint!(data, length(lkey))
+
+    i = j = prevj = 1
+
+    while i <= ll && j <= rr
+        c = rowcmp(lkey, lperm[i], rkey, rperm[j])
+        if c < 0
+            push!(I, lkey[lperm[i]])
+            push!(data, f(ldata[lperm[i]], NA))
+            i += 1
+        elseif c==0
+            i, j = joinequalblock(f, I, data, lkey, rkey,
+                                  ldata, rdata, lperm, rperm, i, j)
+            i += 1
+            j += 1
+        else
+            push!(I, rkey[rperm[j]])
+            push!(data, f(NA, rdata[rperm[j]]))
+            j += 1
+        end
+    end
+    if i <= ll
+        append!(I, lkey[i:ll])
+        append!(data, f.(ldata[i:ll], (NA,)))
+    elseif j <= rr
+        append!(I, rkey[j:rr])
+        append!(data, f.((NA,), rdata[j:rr]))
+    end
+
+    convert(NextTable, I, data, presorted=true)
+end
+
+function outerjoin(left::NextTable, right::NextTable; kwargs...)
+    outerjoin(_right, left, right; kwargs...)
+end
+
+@testset "outerjoin" begin
+    t1 = NextTable([1,2,3,4], [5,6,7,8], primarykey=[1])
+    t2 = NextTable([0,3,4,5], [5,6,7,8], primarykey=[1])
+    t3 = NextTable([0,3,4,4], [5,6,7,8], primarykey=[1])
+    t4 = NextTable([1,3,4,4], [5,6,7,8], primarykey=[1])
+
+    # default: take values from left
+    @test outerjoin(t1, t2, lselect=2, rselect=2) == NextTable([0,1,2,3,4,5], [5,5,6,6,7,8])
+
+    #showl instead of missing row
+    @test outerjoin(+, t1, t2, lselect=2, rselect=2) == NextTable([0,1,2,3,4,5], [NA, NA, NA, 13, 15, NA])
+
+    @test outerjoin(t1, t2) == NextTable([0,1,2,3,4,5], [(0,5), (1,5), (2,6), (3,6), (4,7), (5,8)])
+    @test outerjoin(+, t1, t3, lselect=2, rselect=2)  == NextTable([0,1,2,3,4,4],[NA,NA,NA,13,15,16])
+    @test outerjoin(+, t3, t4, lselect=2, rselect=2) == NextTable([0,1,3,4,4,4,4], [NA, NA, 12,14,15,15,16])
 end
